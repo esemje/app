f3247f81eb8cef1d1433f8e2141cef400753bf24
saumil8200 <saumil8200@gmail.com>
2024-07-20 15:44:21 +0530

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/app/",
-//   plugins: [react()],
-// })
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app/', // Replace with your GitHub Pages repository name
-  plugins: [reactRefresh()],
-});
+  base: "/app/",
+  plugins: [react()],
+})
